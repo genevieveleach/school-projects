def205c2b40fb372a8664b2f594388fda418b26c
Genevieve Leach <genleach@gmail.com>
2017-09-27 21:57:38 -0700

    Implemented multiple simultaneous clients *to project instructions*

--- CS 380/Exercise 1/EchoServer.java
+++ CS 380/Exercise 1/EchoServer.java
@@ -5,53 +5,51 @@ import java.net.Socket;
 
 public final class EchoServer {
 
+    private static ServerSocket serverSocket;
+
     public static void main(String[] args) throws Exception {
 
-        try (ServerSocket serverSocket = new ServerSocket(22222)) {
-            while (true) {
+        Runnable client = () -> {
+            {
                 try {
-                    Socket socket = serverSocket.accept();
-                    ClientThread thread = new ClientThread(socket);
-                    thread.start();
+                    while (true) {
+                        try (Socket socket = serverSocket.accept()) {
+                            String address = socket.getInetAddress().getHostAddress();
+                            System.out.printf("Client connected: %s%n", address);
+                            OutputStream os = socket.getOutputStream();
+                            PrintStream out = new PrintStream(os, true, "UTF-8");
+                            out.printf("Hi %s, thanks for connecting!%n", address);
+
+                            InputStream is = socket.getInputStream();
+                            InputStreamReader isr = new InputStreamReader(is, "UTF-8");
+                            BufferedReader br = new BufferedReader(isr);
+                            String in = null;
+                            while (true) {
+                                in = br.readLine();
+                                if (in.trim().toLowerCase().equals("exit")) {
+                                    break;
+                                }
+                                out.println(in);
+                            }
+                            System.out.printf("Client disconnected: %s%n", address);
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+                    }
                 } catch (Exception e) {
                     e.printStackTrace();
                 }
             }
-        }
-    }
+        };
 
-    static class ClientThread extends Thread {
-
-        Socket client;
-
-        ClientThread(Socket client) {
-            this.client = client;
-        }
-
-        public void run() {
-            try {
-                String address = client.getInetAddress().getHostAddress();
-                System.out.printf("Client connected: %s%n", address);
-                OutputStream os = client.getOutputStream();
-                PrintStream out = new PrintStream(os, true, "UTF-8");
-                out.printf("Hi %s, thanks for connecting!%n", address);
-
-                InputStream is = client.getInputStream();
-                InputStreamReader isr = new InputStreamReader(is, "UTF-8");
-                BufferedReader br = new BufferedReader(isr);
-                String in = null;
-                while (true) {
-                    in = br.readLine();
-                    if (in.trim().toLowerCase().equals("exit")) {
-                        break;
-                    }
-                    out.println(in);
-                }
-                client.close();
-                System.out.printf("Client disconnected: %s%n", address);
-            } catch (Exception e) {
-                e.printStackTrace();
+        serverSocket = new ServerSocket(22222);
+        try {
+            while (true) {
+                Thread newClient = new Thread(client);
+                newClient.start();
             }
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 }
