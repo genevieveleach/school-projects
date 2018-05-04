8c445c1240fa84553c22f2ef38a6ac2ecffa1d6d
Genevieve Leach <genleach@gmail.com>
2018-05-03 23:38:10 -0700

    write input methods; input file should be in "src" directory

--- CS 301/Project 1/src/SPPGauss.java
+++ CS 301/Project 1/src/SPPGauss.java
@@ -1,57 +1,96 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.util.Arrays;
 import java.util.Scanner;
 
 public class SPPGauss {
 
+  private static Scanner kb = new Scanner(System.in);
+
   public static void main(String[] args) {
-    Scanner kb = new Scanner(System.in);
 
     int numEquations = 0;
-/*    while(numEquations <= 0 || numEquations >= 10) {
+    while (numEquations <= 0 || numEquations >= 10) {
       System.out.print("How many linear equations would you like to solve?: ");
       numEquations = kb.nextInt();
       kb.nextLine();
       if (numEquations > 10 || numEquations <= 0) {
         System.out.println("Invalid amount. Please enter coefficients number between 1 and 10.");
       }
-    }*/
-    //TODO: Write user input/file input
-/*    String choice = "";
-    while(!choice.equalsIgnoreCase("enter") || !choice.equalsIgnoreCase("file")) {
+    }
+
+    double[][] coefficients = new double[numEquations][numEquations];
+    double[] rightHandSide = new double[numEquations];
+
+    String choice = "";
+    while (true) {
       System.out.println("Would you like to enter the coefficients of each equation manually or provide coefficients file name?");
       System.out.print("Please type \"enter\" or \"file\" for your choice: ");
       choice = kb.nextLine();
-      if (!choice.equalsIgnoreCase("enter") || !choice.equalsIgnoreCase("file")) {
-        System.out.println("Invalid option.");
+
+      if (choice.equalsIgnoreCase("enter")) {
+        userInput(numEquations, coefficients, rightHandSide);
+        break;
+      } else if (choice.equalsIgnoreCase("file")) {
+        fileInput(numEquations, coefficients, rightHandSide);
+        break;
+      } else {
+        System.out.println("Invalid choice.");
       }
     }
 
-    if(choice.equalsIgnoreCase("enter")) {
-      for (int i = 0; i < numEquations; i++) {
-        System.out.println("Please enter the coefficients of the equation, separated by spaces.");
-      }
-    } else {
-
-    }*/
-
-    // separate input into matrices
-/*    double[][] coefficients = new double[numEquations][numEquations];
-    double[] rightHandSide = new double[numEquations];*/
-
-    double[][] coefficients = {{2,3,-1}, {3,1,2}, {1,0,4}};
-    double[] rightHandSide = {4,5,1};
-    numEquations = coefficients.length;
+    // initialize other arrays
     double[] results = new double[numEquations];
     int[] indices = new int[numEquations];
 
+    // solving process
     gauss(numEquations, coefficients, indices);
     solve(numEquations, coefficients, indices, rightHandSide, results);
 
     // print solution
-    char variable = (char)(90-numEquations+1);
+    char variable = (char) (90 - numEquations + 1);
     for (double aX : results) {
       System.out.println(variable + ": " + aX);
       variable++;
+    }
+  }
+
+  private static void userInput(int numEquations, double[][] coefficients, double[] rightHandSide) {
+    for (int i = 0; i < numEquations; i++) {
+      for (int j = 0; j < numEquations; j++) {
+        System.out.print("Enter the " + j + " coefficient for row " + i + ": ");
+        coefficients[i][j] = kb.nextInt();
+      }
+      System.out.print("Enter the right hand side for row " + i +": ");
+      rightHandSide[i] = kb.nextInt();
+    }
+  }
+
+  private static void fileInput(int numEquations, double[][] coefficients, double[] rightHandSide) {
+    try {
+      System.out.print("Enter the name of the file: ");
+      File file = new File(kb.nextLine());
+      String line = "";
+      FileReader fr = new FileReader(file);
+      BufferedReader br = new BufferedReader(fr);
+      for (int i = 0; i < numEquations; i++) {
+        line = br.readLine();
+        String[] input = line.split(" ");
+        for(int j = 0; j < input.length; j++) {
+          if (j < input.length - 1) {
+            coefficients[i][j] = Double.parseDouble(input[j]);
+          }
+          if (j == input.length - 1) {
+            rightHandSide[i] = Double.parseDouble(input[j]);
+          }
+        }
+      }
+      br.close();
+      fr.close();
+    } catch (Exception e) {
+      e.printStackTrace();
     }
   }
 
@@ -73,6 +112,7 @@ public class SPPGauss {
       }
       s[i] = smax;
     }
+
     for (k = 0; k < numEquations - 1; k++) {
       rmax = 0;
       for (i = k; i < numEquations; i++) {
@@ -82,12 +122,15 @@ public class SPPGauss {
           j = i;
         }
       }
+
       int temp = indices[j];
       indices[j] = indices[k];
       indices[k] = temp;
+
       for (i = k + 1; i < numEquations; i++) {
         xmult = coefficients[indices[i]][k] / coefficients[indices[k]][k];
         coefficients[indices[i]][k] = xmult;
+
         for (j = k + 1; j < numEquations; j++) {
           coefficients[indices[i]][j] = coefficients[indices[i]][j] - (xmult * coefficients[indices[k]][j]);
         }
