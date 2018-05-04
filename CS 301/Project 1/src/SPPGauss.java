be852642f243d2830a203d11875af622cdbce95b
Genevieve Leach <genleach@gmail.com>
2018-05-03 22:49:03 -0700

    renamed variables so they're easier to tell what they are in context

--- CS 301/Project 1/src/SPPGauss.java
+++ CS 301/Project 1/src/SPPGauss.java
@@ -12,13 +12,13 @@ public class SPPGauss {
       numEquations = kb.nextInt();
       kb.nextLine();
       if (numEquations > 10 || numEquations <= 0) {
-        System.out.println("Invalid amount. Please enter a number between 1 and 10.");
+        System.out.println("Invalid amount. Please enter coefficients number between 1 and 10.");
       }
     }*/
     //TODO: Write user input/file input
 /*    String choice = "";
     while(!choice.equalsIgnoreCase("enter") || !choice.equalsIgnoreCase("file")) {
-      System.out.println("Would you like to enter the coefficients of each equation manually or provide a file name?");
+      System.out.println("Would you like to enter the coefficients of each equation manually or provide coefficients file name?");
       System.out.print("Please type \"enter\" or \"file\" for your choice: ");
       choice = kb.nextLine();
       if (!choice.equalsIgnoreCase("enter") || !choice.equalsIgnoreCase("file")) {
@@ -35,28 +35,27 @@ public class SPPGauss {
     }*/
 
     // separate input into matrices
-/*    double[][] a = new double[numEquations][numEquations];
-    double[] b = new double[numEquations];*/
+/*    double[][] coefficients = new double[numEquations][numEquations];
+    double[] rightHandSide = new double[numEquations];*/
 
-    double[][] a = {{2,3,-1}, {3,1,2}, {1,0,4}};
-    double[] b = {4,5,1};
-    numEquations = a.length;
-    System.out.println(numEquations);
-    double[] x = new double[numEquations];
-    int[] l = new int[numEquations];
+    double[][] coefficients = {{2,3,-1}, {3,1,2}, {1,0,4}};
+    double[] rightHandSide = {4,5,1};
+    numEquations = coefficients.length;
+    double[] results = new double[numEquations];
+    int[] indices = new int[numEquations];
 
-    gauss(numEquations, a, l);
-    solve(numEquations, a, l, b, x);
+    gauss(numEquations, coefficients, indices);
+    solve(numEquations, coefficients, indices, rightHandSide, results);
 
     // print solution
     char variable = (char)(90-numEquations+1);
-    for (double aX : x) {
+    for (double aX : results) {
       System.out.println(variable + ": " + aX);
       variable++;
     }
   }
 
-  private static void gauss(int n, double[][] a, int[] l) {
+  private static void gauss(int numEquations, double[][] coefficients, int[] indices) {
     //initialize variables
     int i = 0;
     int j = 0;
@@ -65,53 +64,55 @@ public class SPPGauss {
     double rmax = 0.0;
     double smax = 0.0;
     double xmult = 0.0;
-    double[] s = new double[n];
+    double[] s = new double[numEquations];
 
-    for(i = 0; i < n; i++) {
-      l[i] = i;
-      for (j = 0; j < n; j++) {
-        smax = Math.max(smax, Math.abs(a[i][j]));
+    for(i = 0; i < numEquations; i++) {
+      indices[i] = i;
+      for (j = 0; j < numEquations; j++) {
+        smax = Math.max(smax, Math.abs(coefficients[i][j]));
       }
       s[i] = smax;
     }
-    for (k = 0; k < n - 2; k++) {
+    for (k = 0; k < numEquations - 1; k++) {
       rmax = 0;
-      for (i = k; i < n; i++) {
-        r = Math.abs(a[l[i]][k] / s[l[i]]);
+      for (i = k; i < numEquations; i++) {
+        r = Math.abs(coefficients[indices[i]][k] / s[indices[i]]);
         if (r > rmax) {
           rmax = r;
           j = i;
         }
       }
-      int temp = l[j];
-      l[j] = l[k];
-      l[k] = temp;
-      for (i = k + 1; i < n; i++) {
-        xmult = a[l[i]][k] / a[l[k]][k];
-        a[l[i]][k] = xmult;
-        for (j = k + 1; j < n; j++) {
-          a[l[i]][j] = a[l[i]][j] - xmult * a[l[k]][j];
+      int temp = indices[j];
+      indices[j] = indices[k];
+      indices[k] = temp;
+      for (i = k + 1; i < numEquations; i++) {
+        xmult = coefficients[indices[i]][k] / coefficients[indices[k]][k];
+        coefficients[indices[i]][k] = xmult;
+        for (j = k + 1; j < numEquations; j++) {
+          coefficients[indices[i]][j] = coefficients[indices[i]][j] - (xmult * coefficients[indices[k]][j]);
         }
       }
     }
   }
 
-  private static void solve(int n, double[][] a, int[] l, double[] b, double[] x) {
+  private static void solve(int numEquations, double[][] coefficients, int[] indices, double[] rightHandSide, double[] results) {
     int i;
     int k;
     double sum;
-    for (k = 0; k < n - 2; k++) {
-      for (i = k + 1; i < n; i++) {
-        b[l[i]] = b[l[i]] - a[l[i]][k] * b[l[k]];
+    for (k = 0; k < numEquations - 1; k++) {
+      for (i = k + 1; i < numEquations; i++) {
+        rightHandSide[indices[i]] = rightHandSide[indices[i]] - (coefficients[indices[i]][k] * rightHandSide[indices[k]]);
       }
     }
-    x[n-1] = b[l[n-1]] / a[l[n-1]][n-1];
-    for (i = n - 2; i >= 0; i--) {
-      sum = b[l[i]];
-      for (int j = i + 1; j < n; j++) {
-        sum = sum - a[l[i]][j] * x[j];
+    for(int index = 0; index < numEquations; index++){
+      results[index] = rightHandSide[indices[index]] / coefficients[indices[index]][index];
+    }
+    for (i = numEquations - 1; i > 0; i--) {
+      sum = rightHandSide[indices[i]];
+      for (int j = i + 1; j < numEquations; j++) {
+        sum = sum - (coefficients[indices[i]][j] * results[j]);
       }
-      x[i] = sum / a[l[i]][i];
+      results[i] = sum / coefficients[indices[i]][i];
     }
   }
 }
